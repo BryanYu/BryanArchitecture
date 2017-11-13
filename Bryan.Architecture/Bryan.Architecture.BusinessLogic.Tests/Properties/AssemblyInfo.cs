5d7b9ab97cdf3e5cac1ca1ddaa79e78eb32274a2
BryanYu <ak78912@hotmail.com>
2017-11-13 15:56:43 +0800

    Add BusinessLogic UnitTest Project

---
+++ Bryan.Architecture/Bryan.Architecture.BusinessLogic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bryan.Architecture.BusinessLogic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bryan.Architecture.BusinessLogic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83f8c358-8295-4409-bb4d-7f919c05524d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
