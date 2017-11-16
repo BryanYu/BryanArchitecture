329a3218c544fdc2050758f965412dc081606d33
Bryan <d58526@gmail.com>
2017-11-16 17:49:58 +0800

    NLog - Add File Target

---
+++ Bryan.Architecture/Bryan.Architecture.Utility.UnitTests/Logger/LoggerTestTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bryan.Architecture.Utility.Logger.Enum;
+using NUnit.Framework;
+
+namespace Bryan.Architecture.Utility.UnitTests.Logger
+{
+    [TestFixture]
+    public class LoggerTest
+    {
+        [Test]
+        public void When_Rasie_Trace_Log_To_Console()
+        {
+            try
+            {
+                int zero = 0;
+                int i = 1 / zero;
+            }
+            catch (Exception e)
+            {
+                Bryan.Architecture.Utility.Logger.Logger.Log(LoggerLevel.Debug, e, message: "Test");
+            }
+        }
+    }
+}
