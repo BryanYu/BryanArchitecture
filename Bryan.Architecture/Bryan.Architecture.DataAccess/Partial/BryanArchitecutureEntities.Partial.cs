47986bff46691ae0c9f09c4c6ee4c687c696119d
Bryan <d58526@gmail.com>
2017-11-29 17:21:31 +0800

    EF - Audit Log

--- Bryan.Architecture/Bryan.Architecture.DataAccess/Partial/BryanArchitecutureEntities.Partial.cs
+++ Bryan.Architecture/Bryan.Architecture.DataAccess/Partial/BryanArchitecutureEntities.Partial.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,32 @@ namespace Bryan.Architecture.DataAccess
     {
         public override int SaveChanges()
         {
+            var modifiedEntities = ChangeTracker.Entries().Where(p => p.State == EntityState.Modified).ToList();
+            var now = DateTime.Now;
+
+            foreach (var change in modifiedEntities)
+            {
+                var entityName = change.Entity.GetType().Name;
+                var primaryKey = GetPrimaryKeyValue(change);
+
+                foreach (var prop in change.OriginalValues.PropertyNames)
+                {
+                    var originalValue = change.OriginalValues[prop].ToString();
+                    var currentValue = change.CurrentValues[prop].ToString();
+                    if (originalValue != currentValue)
+                    {
+                    }
+                }
+            }
             return base.SaveChanges();
+        }
+
+        private object GetPrimaryKeyValue(DbEntityEntry entry)
+        {
+            var objectStateEntry =
+                ((IObjectContextAdapter)this).ObjectContext.ObjectStateManager.GetObjectStateEntry(entry.Entity);
+
+            return objectStateEntry.EntityKey.EntityKeyValues[0].Value;
         }
     }
 }
